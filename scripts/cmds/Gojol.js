811328053d51cdad0baaad8b76629da86383add0
SK-SIDDIK-143 <abubakkarsiddique13570@gmail.com>
2024-09-23 11:42:07 +0600

    Update Gojol.js

--- scripts/cmds/Gojol.js
+++ scripts/cmds/Gojol.js
@@ -1,27 +1,26 @@
 module.exports = {
   config: {
-    name: "gojol",
-    aliases: ["islamicaudio", "lyricaledit"],
-    version: "1.0",
-    author: "SIDDIK",
-    countDown: 5,
-    role: 0,
-    shortDescription: "get islamic audio",
-    longDescription: "it will send you islamic audio from lyrics edit vibe group",
-    category: "music",
-    guide: "{pn} bd",
-  },
+  name: "gojol",
+  version: "1.0.0",
+  role: 0,
+  author: "SIDDIK",
+  countDown: 5,
+  shortDescription: "Made By Siddik",
+  category: "user", 
+  guide: "{pn} bd",
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
  
-  sentaudio: [],
- 
-  onStart: async function ({ api, event, message }) {
-    const senderID = event.senderID;
- 
-    const loadingMessage = await message.reply({
-      body: "Loading Islamic  Gojol... Please wait! 🕐",
-    });
- 
-    const link = [ 
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["--Islamic Gojol By Sk Siddik 🕌--"];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [
    "https://drive.google.com/uc?id=1xjyq3BrlW3bGrp8y7eedQSuddCbdvLMN",
    
    "https://drive.google.com/uc?id=1CCQqJVqvFsgyAd4ZjZB0BJ3lGN4Kc2l2",
@@ -35,27 +34,8 @@ module.exports = {
    "https://drive.google.com/uc?id=1ySwrEG6xVqPdY5BcBP8I3YFCUOX4jV9e",
    
    "https://drive.google.com/uc?id=1CESeRi5Ue4HR6GSDfYJrREGGcsvYJvAB"];
+     var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/26.mp3")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/26.mp3"));    
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/26.mp3")).on("close",() => callback());
+   };
  
-    const availableAudio = link.filter(audio => !this.sentAudio.includes(audio));
  
-    if (availableAudio.length === 0) {
-      this.sentAudio = [];
-    }
- 
-    const randomIndex = Math.floor(Math.random() * availableAudio.length);
-    const randomAudio = availableAudio[randomIndex];
- 
-    this.sentAudio.push(randomAudio);
- 
-    if (senderID !== null) {
-      message.reply({
-        body: '╰‣ Islamic Gojol By Sk Siddik',
-        attachment: await global.utils.getStreamFromURL(randomAudio),
-      });
- 
-      setTimeout(() => {
-        api.unsendMessage(loadingMessage.messageID);
-      }, 5000);
-    }
-  },
-};

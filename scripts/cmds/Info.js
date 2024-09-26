b8133c2d02b5eac3ca087045e4f2f3fc4d608480
SK.SIDDIK.KHAN <siddik0351@gmail.com>
2024-09-26 08:24:22 +0600

    Create Info.js

---
+++ scripts/cmds/Info.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const moment = require('moment-timezone');
+ 
+module.exports = {
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "SIDDIK",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const botName = "𝐒𝐊_𝐒𝐈𝐃𝐃𝐈𝐊_⓿❼";
+		const botPrefix = ".";
+		const authorName = "𝐒𝐊_𝐒𝐈𝐃𝐃𝐈𝐊";
+		const ownAge = "18";
+		const authorFB = "https://www.facebook.com/rxsiddik1";
+		const urls = JSON.parse(fs.readFileSync('aryan.json'));
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Jakarta');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+ 
+		message.reply({
+			body: `  𝐎𝐖𝐍𝐄𝐑-𝐈𝐍𝐅𝐎  \n
+🤖 Bot Name: ${botName}
+🚀 Prefix: ${botPrefix}
+👤 Owner: ${authorName}
+🔆 Age: ${ownAge}
+🍒 authorFb: ${authorFB}
+⏱️ seconds: ${seconds}
+🕰️ minutes: ${minutes}
+🛸 hours: ${hours}
+🌒 days: ${days}
+🌧️ date: ${date}
+☔ Time: ${time}
+⏰ uptime: ${uptimeString}
+ `,
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
+};

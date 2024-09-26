a208e71d0e468e7e564102909f50283283783f23
SK.SIDDIK.KHAN <siddik0351@gmail.com>
2024-09-26 09:31:52 +0600

    Update approve.js

--- scripts/cmds/approve.js
+++ scripts/cmds/approve.js
@@ -1,30 +1,30 @@
 const fs = require("fs");
-
+ 
 module.exports = {
 	config: {
 		name: "approved",
-		author: "Jun",
+		author: "SIDDIK",
 		countDown: 5,
-		role: 0,
+		role: 2,
 		category: "owner",
 		shortDescription: {
 			en: ""
 		}
 	},
-
+ 
 	onLoad: async function() {
 		const dataPath = __dirname + "/cache/approvedThreads.json";
 		const pendingPath = __dirname + "/cache/pendingThreads.json";
-
+ 
 		if (!fs.existsSync(dataPath)) {
 			fs.writeFileSync(dataPath, JSON.stringify([]));
 		}
-
+ 
 		if (!fs.existsSync(pendingPath)) {
 			fs.writeFileSync(pendingPath, JSON.stringify([]));
 		}
 	},
-
+ 
 	onStart: async function({ event, api, args }) {
 		const { threadID, messageID, senderID } = event;
 		const dataPath = __dirname + "/cache/approvedThreads.json";
@@ -33,34 +33,34 @@ module.exports = {
 		let pending = JSON.parse(fs.readFileSync(pendingPath));
 		let msg = "";
 		let idBox = args[0] || threadID;
-
+ 
 		if (args[0] == "list") {
 			msg = "LIST OF APPROVED BOXES! ";
 			let count = 0;
-
+ 
 			for (const e of data) {
 				msg += `${count += 1}. ID: ${e}`;
 			}
-
+ 
 			api.sendMessage(msg, threadID, messageID);
 		} else if (args[0] == "del") {
 			idBox = args[1] || event.threadID;
-
+ 
 			if (isNaN(parseInt(idBox))) {
 				api.sendMessage("Not a number.", threadID, messageID);
 				return;
 			}
-
+ 
 			if (!data.includes(idBox)) {
 				api.sendMessage("The box was not approved before! ", threadID, messageID);
 				return;
 			}
-
+ 
 			api.sendMessage(`Box ${idBox} has been removed from bot permission list `, threadID, () => {
 				if (!pending.includes(idBox)) {
 					pending.push(idBox);
 				}
-
+ 
 				data.splice(data.indexOf(idBox), 1);
 				fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
 				fs.writeFileSync(pendingPath, JSON.stringify(pending, null, 2));
@@ -68,12 +68,12 @@ module.exports = {
 		} else if(args[0] == "pending") {
 			msg = "LIST OF BOXES WAITING TO BE APPROVED!";
 			let count = 0;
-
+ 
 			for (const e of pending) {
 				const name = (await api.getThreadInfo(e)).name || "Nhóm Chat";
 				msg += `${count += 1}. ${name}\ID: ${e}`;
 			}
-
+ 
 			api.sendMessage(msg, threadID, messageID);
 		} else if (isNaN(parseInt(idBox))) {
 			api.sendMessage("Invalid ID entered ", threadID, messageID);
@@ -94,3 +94,4 @@ module.exports = {
 		}
 	}
 };
+ 

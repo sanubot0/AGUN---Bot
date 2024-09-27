ac4cdf6bfdab878b2f7667ff5d87181949b6bede
Badboy-404 <108590422+badboysabbir8@users.noreply.github.com>
2024-09-27 10:53:12 +0600

    Update uid.js

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -34,11 +34,11 @@ module.exports = {
 		}
 	},
 
-	onStart: async function ({ message, event, args, getLang }) {
+	onStart: async function ({api, message, event, args, getLang }) {
 		if (event.messageReply)
-			return message.reply(event.messageReply.senderID);
+			return api.shareContact(event.messageReply.senderID, event.messageReply.senderID, event.threadID);
 		if (!args[0])
-			return message.reply(event.senderID);
+			return api.shareContact(event.senderID, event.senderID, event.threadID);
 		if (args[0].match(regExCheckURL)) {
 			let msg = '';
 			for (const link of args) {
